3f5a438fe6b05d5ff240d6031eec42c953bc179e
Eduardo <eoalcantara@gmail.com>
2020-09-18 19:07:59 -0300

    FiC

--- Wishlist.Api/Controllers/WeatherForecastController.cs
+++ Wishlist.Api/Controllers/WeatherForecastController.cs
@@ -27,7 +27,7 @@ namespace Wishlist.Api.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 15).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
