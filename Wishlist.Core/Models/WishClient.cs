d80449539c440a55f77a39a3b57d6b6c6adae160
Eduardo <eoalcantara@gmail.com>
2020-09-19 12:14:51 -0300

    Adicionado o modelo desejo do cliente

---
+++ Wishlist.Core/Models/WishClient.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using System;
+using Wishlist.Core.Validators;
+
+namespace Wishlist.Core.Models
+{
+    public class WishClient : BaseModel<Guid>
+    {
+        public Client Client { get; set; }
+        public Product Product { get; set; }
+
+        public WishClient()
+        {
+
+        }
+
+        public WishClient(Client client, Product product)
+        {
+            Id = Guid.NewGuid();
+            Client = client;
+            Product = product;
+        }    
+
+       
+
+        public static WishClient WishClientBuilder(Client client, Product product)
+        {
+            var wishClient = new WishClient(client,  product);
+            
+            wishClient.IsValid();
+
+            return wishClient;
+        }
+
+        public override bool IsValid()
+        {
+            var validator = new WishClientValidator();
+            var result = validator.Validate(this, rst =>
+            {
+                rst.ThrowOnFailures();
+            });
+
+            return result.IsValid;
+        }
+    }
+}
---
+++ Wishlist.Core/Validators/WishClientValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Wishlist.Core.Models;
+
+namespace Wishlist.Core.Validators
+{
+    public class WishClientValidator : AbstractValidator<WishClient>
+    {
+        public WishClientValidator()
+        {
+            RuleFor(x => x.Client)
+                .NotNull()
+                .SetValidator(new ClientValidator())
+                .WithMessage("Cliente é obrigatório!");
+
+
+            RuleFor(x => x.Product)
+             .NotNull()
+             .SetValidator(new ProducttValidator())
+             .WithMessage("Produto é obrigatório!");
+
+
+        }
+    }
+}
