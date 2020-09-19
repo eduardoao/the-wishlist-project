993cf07d2538771c7d0634f0b7cc6bdd202f6d42
Eduardo <eoalcantara@gmail.com>
2020-09-19 16:28:06 -0300

    Swagger adicionado

--- Wishlist.Api/Startup.cs
+++ Wishlist.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Wishlist.Core.Interfaces.Repositorys;
 using Wishlist.Data;
 
@@ -31,6 +32,46 @@ namespace Wishlist.Api
 
             services.AddTransient<IRepositoryClient, RepositoryClient>();
             services.AddTransient<IRepositoryProduct, RepositoryProduct>();
+
+            // Configurando o serviço de documentação do Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Wishlist.Api",
+                        Version = $"v1 - {DateTime.Now}",
+                        Description = "API Wishlist.Api",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Wishlist.Api",
+                            Url = new Uri("https://the-wishlist-project.herokuapp.com/")
+                        }
+                    });
+
+                c.AddSecurityDefinition("Bearer",
+                new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Informe o token JWT",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                                {
+                                    {
+                                        new OpenApiSecurityScheme
+                                        {
+                                            Reference = new OpenApiReference
+                                            {
+                                                Type = ReferenceType.SecurityScheme,
+                                                Id = "Bearer"
+                                            }
+                                        }, new List<string>()
+                                    }
+                                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +87,12 @@ namespace Wishlist.Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API de Lista de desejos");
+            });
 
             app.UseEndpoints(endpoints =>
             {
